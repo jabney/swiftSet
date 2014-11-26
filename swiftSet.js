5a3754e1b9fb2362380a68f7c0f6b3d88ef967d8
James Abney <james.abney@gmail.com>
2014-11-26 00:46:43 -0800

    Set is now independent of Histogram

--- swiftSet.js
+++ swiftSet.js
@@ -17,18 +17,18 @@ var
 // Shortcuts
 slice = Array.prototype.slice,
 
+// Return the type of built-in objects via toString.
+typeOf = (function() {
+  var reType = /\[object (\w+)\]/; 
+  return function(obj) {
+    return reType.exec(toString.call(obj))[1];
+  };
+})(),
+
 // Encode object type for key generation.
 encodeObjType = (function() {
 
   var toString = Object.prototype.toString,
-
-  // Return the type of built-in objects via toString.
-  typeOf = (function() {
-    var reType = /\[object (\w+)\]/; 
-    return function(obj) {
-      return reType.exec(toString.call(obj))[1];
-    };
-  })(),
 
   // A list of built-in types.
   types = ['Null','Undefined','Array','Boolean','Number','String','Object',
@@ -106,110 +106,42 @@ function isWrapped(obj) {
 // {id: f9de, uid: funciton() { return this.id; }}
 // ---------------------------------------------------------------
 
-function Set(a, key) {
-  var hist = new Histogram(a, key), cachedItems = hist.items();
+function Set(a, hashFn) {
+  var mutable = false;
 
-  // Normalize histograms a and b, then merge and iterate.
-  // Returns a filtered array based on the given evaluator;
-  // absent an evaluator, returns a merged histogram.
+  // Create the set's backing object.
+  this.hist = Object.create(null);
+
+  // Return either the identity function, or the 
+  // given hash function passed to the constructor.
+  this.uid = (function() {
+    return typeof hashFn === 'undefined' ?
+      // The identity function.
+      function() {
+        return this;
+      } :
+      // User-specified global key retriever.
+      function() {
+        return hashFn.call(this);
+      };
+  })();
+
+  // Initialize with array if supplied in constructor.
+  a && this.add.apply(this, a);
+
+  // Process set operations. Calls into Set.process.
   this.process = function(b, evaluator) {
-    var items = (b instanceof Set) ? b.items() : b,
-    out = [],
-    a = new Histogram(cachedItems, key), 
-    b = new Histogram(items, key).normalize(2);
-    // Merge the histograms and evaluate the contents.
-    a.merge(b);
-    if(evaluator) {
-      a.each(function(item, freq) {
-        evaluator(freq) && out.push(item); 
-      });
-      return out;
-    } else {
-      return a;
-    }
-  };
+    var b = (b instanceof Set) ? b.items() : b, result;
+    hashFn && Set.pushUid(hashFn);
+    result = Set.process(this.items(), b, evaluator);
+    hashFn && Set.popUid();
+    mutable && typeOf(result) === 'Array' && this.clear(result);
+    return result;
+  }
 
-  // Iterates through the items in this set.
-  this.each = function(action, context) {
-    return hist.each(function(item) {
-      return action.call(this, item);
-    }, context);
-  };
-
-  // An array of items comprising the set.
-  this.items = function() {
-    return hist.items();
-  };
-
-  // An array of unwrapped items.
-  this.unwrap = function() {
-    return hist.unwrap();
-  };
-
-  // Encodes key/type pairs for each element in the set.
-  this.keyify = function() {
-    var uid = [], typeCode;
-    hist.each(function(item, freq, key) {
-      uid.push(key + ',');
-    });
-    return '{' + uid.sort().join('').slice(0, -1) + '}';
-  };
-
-  // Conversion of this set to a representative string.
-  this.toString = function() {
-    return this.keyify();
-  };
-
-  // Add an item or items to this set.
-  this.add = function() {
-    hist.addItems(slice.call(arguments, 0));
-    cachedItems = hist.items();
-    return this;
-  };
-
-  // Remove an item or items from this set.
-  this.remove = function() {
-    hist.removeItems(slice.call(arguments, 0));
-    cachedItems = hist.items();
-    return this;
-  };
-
-  // Add multiple items to the set via an array of items.
-  this.addItems = function(items) {
-    hist.addItems(items);
-    cachedItems = hist.items();
-    return this;
-  };
-
-  // Remove multiple items from the set via an array of items.
-  this.removeItems = function(items) {
-    hist.removeItems(items);
-    cachedItems = hist.items();
-    return this;
-  };
-
-  // Returns a copy of this set.
-  this.copy = function(b) {
-    return b ? new Set(b, key) :
-      new Set(cachedItems, key);
-  };
-
-  // The number of unique elements in the set.
-  this.size = function() {
-    return hist.size();
-  };
-
-  // Determines if a value is present in the set.
-  this.has = function(item) {
-    return hist.has(item);
-  };
-
-  // Remove all items from a set. Add mew values if given.
-  // TODO: unit tests.
-  this.clear = function(a) {
-    hist.clear(a);
-    cachedItems = hist.items();
-    return this;
+  // Create a clone of this set.
+  this.clone = function() {
+    return new Set(this.items(), hashFn);
   };
 
 }
@@ -219,6 +151,112 @@ Set.wrapObj = wrapObj;
 Set.isWrapped = isWrapped;
 
 Set.prototype = {
+
+  // Add one or more items to the set. add('a', 'b', 'c')
+  add: function() {
+    slice.call(arguments, 0).forEach(function(arg) {
+      var key = this.uid.call(arg), entry = this.hist[key];
+      if (!entry) {
+        entry = Object.create(null);
+        entry.item = arg;
+        entry.freq = 1;
+        this.hist[key] = entry;
+        ;
+      }
+    }, this);
+    return this;
+  },
+
+  // Remove one or more items from the set. remove('b', 'c')
+  remove: function() {
+    slice.call(arguments, 0).forEach(function(arg) {
+      var key = this.uid.call(arg);
+      if (this.hist[key]) {
+        delete this.hist[key];
+      }
+    }, this);
+    return this;
+  },
+
+  // Add multiple item to the histogram via an array of item.
+  addItems: function(a) {
+    this.add.apply(this, a);
+    return this;
+  },
+
+  // Remove multiple items from the histogram via an array of items.
+  removeItems: function(a) {
+    this.remove.apply(this, a);
+    return this;
+  },
+
+  // Clear items from this set. Optionally initialize
+  // with an array of items.
+  clear: function(a) {
+    this.hist = Object.create(null);
+    a && this.addItems(a);
+    return this;
+  },
+
+  // Iterate over items in the set. Return true to exit early.
+  each: function(action, context) {
+    for (var key in this.hist) {
+      if (action.call(
+        context, this.hist[key].item
+     )) break;
+    }
+    return this;
+  },
+
+  // Map items in this set to another array of the same length.
+  map: function(action, context) {
+    var out = [];
+    this.each(function(item) {
+      out.push(action.call(this, item));
+    }, context);
+    return out;
+  },
+
+  // Return the items in this set.
+  items: function() {
+    return this.map(function(item) {
+      return item;
+    });
+  },
+
+  // Encodes key/type pairs for each element in the set.
+  keyify: function() {
+    var keys = [], typeCode;
+    this.each(function(item) {
+      var key = this.uid.call(item);
+      keys.push(key + ':' + encodeObjType(item) + ',');
+    }, this);
+    return '{' + keys.sort().join('').slice(0, -1) + '}';
+  },
+
+  // An array of unwrapped items.
+  unwrap: function() {
+    return this.map(function(item) {
+      return isWrapped(item) ? item.item : item;
+    }, this);
+  },
+
+  // Conversion of this set to a representative string.
+  toString: function() {
+    return this.keyify();
+  },
+
+  // The number of unique elements in the set.
+  size: function() {
+    return this.items().length;
+  },
+
+  // Determines if a value is present in the set.
+  has: function(item) {
+    var key = this.uid.call(item),
+    item = this.hist[key] && this.hist[key].item;
+    return item && true || false;
+  },
 
   // ---------------------------------------------------------------
   // Set operations - these operatons make use of sets 'a' (the set based
@@ -260,8 +298,13 @@ Set.prototype = {
 
   // Returns true if given set is equivalent to this set. 
   equals: function(b) {
-    var h = this.process(b);
-    return h.min() === 3 && h.max() === 3;
+    var h = this.process(b), k,
+    max = 0, min = Math.pow(2, 53);
+    for (k in h) {
+      max = Math.max(max, h[k].freq);
+      min = Math.min(min, h[k].freq);
+    }
+    return min === 3 && max === 3;
   },
 
   constructor: Set
@@ -391,337 +434,6 @@ Set.prototype = {
 
 // Export
 swiftSet.Set = Set;
-
-// ---------------------------------------------------------------
-// Histogram - a discrete histogram designed as a backing object
-// for the Set constructor, but it can be used to record frequencies
-// of arrays of values, characters in a string, or just about anything.
-//
-// Example usage: 
-// 
-// var str = "We hold these truths to be self-evident, that all men \
-// are created equal, that they are endowed by their Creator with \
-// certain unalienable Rights, that among these are Life, Liberty \
-// and the pursuit of Happiness.";
-// str = str.replace(/[.,]/g, '');
-
-// var vc = [],
-// hist = new Histogram(str.toLowerCase())
-//   .each(function(value, freq) {
-//     vc.push(value, freq);
-//   });
-
-// console.log(vc);
-// // ["w", 3, "e", 28, " ", 34, "h", 13, "o", 6, "l", 9, "d", 6, ...]
-
-// vc = [];
-// hist = new Histogram(str.toLowerCase().split(' '))
-//   .each(function(value, freq) {
-//     vc.push(value, freq);
-//   });
-
-// console.log(vc);
-// // [..., "these", 2, "truths", 1, "to", 1, "be", 1, "self-evident", 1,
-// // "that", 3, "all", 1, "men", 1, "are", 3, "created", 1, "equal", 1, ...]
-// ---------------------------------------------------------------
-
-function Histogram(items, key) {
-  var hist = Object.create(null), _length = 0, _max = 0,
-  // If 'items' is a string, convert it to an array.
-  items = typeof items === 'string' ? items.split('') : items,
-  // Generate a uid function depending on whether key is specified
-  // and whether it's a function or value.
-  uid = (function() {
-    return typeof key === 'undefined' ?
-      // If key is not defined in the constructor, look for a property 
-      // on the object named 'hkey' and use it as a value or function.
-      // This fallback is necessary so that objects can be mixed with
-      // other items in the histogram.
-      function(obj) {
-        var key = obj.hkey, type = typeof key;
-        return type === 'undefined' ? obj :
-          type === 'function' ? key.call(obj) : key;
-      } :
-      // If key is specified in the constructor, use it as a value
-      // or a function depending on its type. In this instance,
-      // all entries in the histogram must be objects with this
-      // property present.
-      typeof key === 'function' ?
-        function(obj) { return key.call(obj); } :
-        function(obj) { return obj[key]; }
-  })();
-
-  // Initialize histogram with given items.
-  items && items.forEach(function(item) {
-    add(item);
-  });
-
-  // Add a single item to the histogram. If 'freq' is specified,
-  // set the item's frequency; this facilitates the merge operation.
-  function add(item, freq) {
-    var key = uid(item), entry = hist[key],
-    freq = typeof freq === 'undefined' ? 1 : freq;
-    // If the entry already exists, update the frequency.
-    if (entry) {
-      entry.freq += freq;
-    // Otherwise create a new entry and update the length.
-    } else {
-      hist[key] = entry = { item: item, freq: freq };
-      _length++;
-    }
-    _max = Math.max(_max, entry.freq);
-    return this;
-  }
-
-  // Remove a single item from the histogram.
-  function remove(item) {
-    var key = uid(item);
-    if (hist[key]) {
-      delete hist[key];
-      _length--;
-    }
-    return this;
-  }
-
-  // Add multiple item to the histogram via an array of item.
-  this.addItems = function(items) {
-    items.forEach(function(item) {
-      add(item);
-    });
-    return this;
-  };
-
-  // Remove multiple items from the histogram via an array of items.
-  this.removeItems = function(items) {
-    items.forEach(function(item) {
-      remove(item);
-    });
-    return this;
-  };
-
-  // Return a copy of this histogram. If b is given, the copy
-  // will have those values instead.
-  this.copy = function(b) {
-    return b ? new Histogram(b, key) :
-      new Histogram(null, key).merge(this);
-  };
-
-  // By default, sets all the histogram's frequencies to 1. If 'freq' 
-  // is specified, sets all the histogram's frequencies to the given freq.
-  this.normalize = function(freq) {
-    var key, freq = typeof freq === 'undefined' ? 1 : freq;
-    for (key in hist) {
-      hist[key].freq = freq;
-    }
-    _max = freq;
-    return this;
-  };
-
-  // Iterate through each of the histogram's entries. If action
-  // returns a truthy item, the loop terminates.
-  this.each = function(action, context) {
-    for (var key in hist) {
-      if (action.call(
-        context, hist[key].item, hist[key].freq, key
-     )) break;
-    }
-    return this;
-  };
-
-  // Returns true if the specified item exists in the histogram.
-  this.has = function(item) {
-    var key = uid(item);
-    return hist[key] === undefined ? false : true;
-  };
-
-  // Returns the number of entries in the histogram.
-  this.size = function() {
-    return _length;
-  };
-
-  // Returns the maximum frequency of items in the histogram.
-  this.max = function() {
-    return _max;
-  };
-
-  // Returns the frequency corresponding to the given value.
-  this.freq = function(item) {
-    var key = uid(item);
-    return hist[key] === undefined ? 0 : hist[key].freq;
-  };
-
-  // Merges the given histogram into this one.
-  this.merge = function(h) {
-    h.each(function(item, freq) {
-      add(item, freq);
-    });
-    return this;
-  };
-
-  // Clear the histogram of all items. Add array of
-  // new items if given.
-  // TODO: unit tests.
-  this.clear = function(a) {
-    hist = Object.create(null);
-    _length = 0;
-    _max = 0;
-    a && this.addItems(a);
-    return this;
-  };
-
-}
-
-// Helpers
-Histogram.wrapObj = wrapObj;
-Histogram.isWrapped = isWrapped;
-
-Histogram.prototype = {
-
-  // Add one or more items to the histogram.
-  add: function() {
-    this.addItems(slice.call(arguments, 0));
-    return this;
-  },
-
-  // Remove one or more items from the histogram.
-  remove: function() {
-    this.removeItems(slice.call(arguments, 0));
-    return this;
-  },
-
-  // Returns an array of the histogram's items.
-  items: function() {
-    return this.map(function(item) {
-      return item;
-    });
-  },
-
-  // Returns an array of the histogram's frequencies.
-  frequencies: function() {
-    return this.map(function(item, freq) {
-      return freq;
-    });
-  },
-
-  // Returns an array of the histogram's keys.
-  keys: function() {
-    return this.map(function(item, freq, key) {
-      return key;
-    });
-  },
-
-  // Returns an array of items, but unwraps any wrapped objects.
-  unwrap: function() {
-    return this.map(function(item) {
-      return isWrapped(item) ? item.item : item;
-    });
-  },
-
-  // Returns the minimum frequency in the histogram.
-  min: function() {
-    return Math.min.apply(null, this.frequencies());
-  },
-
-  // Return items that have frequencies greater than 
-  // or equal to the given frequency.
-  ge: function(freq) {
-    var out = [];
-    this.each(function(value, f) {
-      f >= freq && out.push(value);
-    });
-    return out;
-  },
-
-  // Return items that have frequencies less than or
-  // equal to the given frequency.
-  le: function(freq) {
-    var out = [];
-    this.each(function(value, f) {
-      f <= freq && out.push(value);
-    });
-    return out;
-  },
-
-  // Return items that have exactly the given frequency.
-  eq: function(freq) {
-    var out = [];
-    this.each(function(value, f) {
-      f === freq && out.push(value);
-    });
-    return out;
-  },
-
-  // Returns the sum of all entry's frequencies.
-  total: function() {
-    return this.reduce(function(sum, curr) {
-      return sum + curr;
-    }, 0);
-  },
-
-  // Returns the average value of frequencies in the histogram.
-  average: function() {
-    return this.total() / this.size();
-  },
-
-  // Returns the Shannon entropy of the histogram in bits per symbol.
-  entropy: function() {
-    var size = this.total();
-    return this.reduce(function(sum, freq) {
-      var p = freq/size;
-      return sum - p * Math.log2(p);
-    }, 0);
-  },
-
-  // Returns the total number of bits in the histogram.
-  // If 'uniform' is truthy, assume uniform probability of values.
-  // If 'uniform' > 1 assume uniform is a base.
-  bits: function(uniform) {
-    return this.total() * (uniform ?
-      Math.log2(uniform > 1 ? uniform : this.size()) :
-      this.entropy());
-  },
-
-  // Determine if another histogram is equivalent to this one.
-  equals: function(histogram) {
-    return this.keyify() === histogram.keyify();
-  },
-
-  // Returns an array of user-transformed entries.
-  map: function(action, context) {
-    var out = [];
-    this.each(function(item, freq, key) {
-      out.push(action.call(this, item, freq, key));
-    }, context);
-    return out;
-  },
-
-  // Reduces all frequencies in the histogram to a single value.
-  reduce: function(action, initial) {
-    return this.frequencies().reduce(function(prev, curr) {
-      return action(prev, curr);
-    }, initial);
-  },
-
-  // Encodes key/frequency pairs for each element in the histogram.
-  // Histograms can be considered equivalent if their keys are equal.
-  keyify: function() {
-    var uid = [], typeCode;
-    this.each(function(item, freq, key) {
-      uid.push(key + ':' + freq + ',');
-    });
-    return '{' + uid.sort().join('').slice(0, -1) + '}';
-  },
-
-  // Conversion of this histogram to a representative string.
-  toString: function() {
-    return this.keyify();
-  },
-
-  constructor: Histogram
-};
-
-// Export
-swiftSet.Histogram = Histogram;
 
 })(window.swiftSet = window.swiftSet || {});
 
